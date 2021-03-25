3f2ebe2a025c2448f21bb77bd3b46787ae4727e7
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-03-24 22:42:38 -0600

    deleting .ds_store file update bootstrap file

--- js/scripts.js
+++ js/scripts.js
@@ -19,21 +19,37 @@ function divide(number1, number2) {
 
 // User interface logic:
 
-$(document).ready(function() {
-  $("form#add").submit(function(event) {
-   event.preventtDefault();
+// $(document).ready(function () {
+
+//   $("form#add").click(function (event) {
+//     event.preventtDefault();
+//     const number1 = parseInt($("#add1").val());
+//     const number2 = parseInt($("#add2").val());
+
+//     $("addition").click(function () {
+//       const result = add(number1, number2);
+//       $("#output").text(result);
+//     });
+//     $("subtraction").click(function () {
+//       const result = subtract(number1, number2);
+//       $("output").text(result);
+//     });
+//     $("multiply").click(function () {
+//       const result = multiply(number1, number2);
+//       ("output").text(result);
+//     });
+//     $("divide").click(function () {
+//       const result = divide(number1, number2);
+//       ("output").text(result);
+//     });
+//   });
+// });
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    alert(result);
   });
-
-  // $("form#subtract").submit(function(event) {
-  //   event.preventDefault();
-  //   const number1 = parseInt($("#subtract1").val());
-  //   const number2 = parseInt($("#subtract2").val());
-  //   const result = subtract(number1, number2);
-  //   $("#output").text(result);
-  
-  // });
 });
